b7a5ac7416f7658374eb3773a0366974994a71aa
Prohell1 <prohell@outlook.hu>
2022-03-28 13:32:44 +0200

    test

---
+++ greetings.py
@@ -0,0 +1,15 @@
+name = input("What's your name? ")
+print(f'Hello {name}, I am your phone book.')
+
+num_string = input("How old are you? ")
+x = num_string.isnumeric()
+if x == False:
+    print("That doesn't seem to be an integer.")
+    exit()
+age = int(num_string)
+if age <= 18:
+    print("You are so young! Life is ahead of you!")
+elif age < 40:
+    print("That's a nice age!")
+else:
+    print("You must be very wise!")
