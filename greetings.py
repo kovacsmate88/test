e658e2d49a1c364af20314fa8f4aae6c5bfdd893
Prohell1 <prohell@outlook.hu>
2022-03-28 13:36:23 +0200

    me push, you push??

--- greetings.py
+++ greetings.py
@@ -5,11 +5,10 @@ num_string = input("How old are you? ")
 x = num_string.isnumeric()
 if x == False:
     print("That doesn't seem to be an integer.")
-    exit()
 age = int(num_string)
 if age <= 18:
     print("You are so young! Life is ahead of you!")
 elif age < 40:
-    print("That's a nice age!")
+    print(3+2)
 else:
     print("You must be very wise!")
